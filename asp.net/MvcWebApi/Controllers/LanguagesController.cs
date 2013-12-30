cefd016fd15aed83fdb19b06ead75547cc6b649c
mauro_a_o <mauro.anselmo.oliveira@gmail.com>
2013-12-30 00:26:16 -0200

    update LanguagesController.cs

--- asp.net/MvcWebApi/Controllers/LanguagesController.cs
+++ asp.net/MvcWebApi/Controllers/LanguagesController.cs
@@ -25,45 +25,77 @@ namespace MvcWebApi.Controllers
 		/// </summary>
 		/// <param name="index">Index of language</param>
 		/// <returns>returns language name</returns>
-		public string Get(int index)
+		public HttpResponseMessage Get(int index)
 		{
 			var languages = LanguagesRepository.GetLanguages();
 
 			if (index < 0 || index >= languages.Count || languages.Count == 0)
 			{
-				throw new HttpResponseException(HttpStatusCode.NotFound);
+				return Request.CreateErrorResponse(HttpStatusCode.NotFound, "language not found");
 			}
 
-			return languages[index];
+			return Request.CreateResponse<string>(HttpStatusCode.OK, languages[index]);
 		}
 
 		/// <summary>
 		/// Inserts a new language. Verb = POST. Path = api/languages
 		/// </summary>
 		/// <param name="language">language object</param>
-		public void Post([FromBody]string languageName)
+		public HttpResponseMessage Post([FromBody]string languageName)
 		{
 			if (string.IsNullOrEmpty(languageName))
 			{
-				throw new HttpResponseException(HttpStatusCode.BadRequest);
+				return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "languageName can not be null/empty");
 			}
+
 			LanguagesRepository.GetLanguages().Add(languageName);
+
+			var resp = Request.CreateResponse(HttpStatusCode.Created);
+			resp.Headers.Location = new Uri(string.Concat(Request.RequestUri, LanguagesRepository.GetLanguages().Count - 1));
+
+			return resp;
+		}
+
+		/// <summary>
+		/// Get language by its index and changes its name. Verb = PUT. Path = api/language/5
+		/// </summary>
+		/// <param name="index">index of language</param>
+		/// <param name="languageName"></param>
+		public HttpResponseMessage Put(int index, [FromBody]string languageName)
+		{
+			var languages = LanguagesRepository.GetLanguages();
+
+			if (index < 0 || index >= languages.Count || languages.Count == 0)
+			{
+				return Request.CreateErrorResponse(HttpStatusCode.NotFound, "language not found");
+			}
+
+			if (string.IsNullOrEmpty(languageName))
+			{
+				return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "languageName can not be null/empty");
+			}
+
+			languages[index] = languageName;
+
+			return Request.CreateResponse(HttpStatusCode.OK);
 		}
 
 		/// <summary>
 		///  Removes language by its index. Verb = DELETE. Path = api/languages/5
 		/// </summary>
 		/// <param name="index">index of language</param>
-		public void Delete(int index)
+		public HttpResponseMessage Delete(int index)
 		{
 			var languages = LanguagesRepository.GetLanguages();
 
 			if (index < 0 || index >= languages.Count || languages.Count == 0)
 			{
-				throw new HttpResponseException(HttpStatusCode.NotFound);
+				return Request.CreateErrorResponse(HttpStatusCode.NotFound, "language not found");
 			}
 
 			languages.RemoveAt(index);
+
+			return Request.CreateResponse(HttpStatusCode.OK);
 		}
 	}
 }
